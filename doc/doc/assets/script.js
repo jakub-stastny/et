8030affa0f0ee83444749136b3d3ba7dc18137b4
Jakub Šťastný <jakub-stastny@users.noreply.github.com>
2023-03-16 15:24:34 -0600

    WIP

--- doc/doc/assets/script.js
+++
@@ -1,7 +0,0 @@
-const $ = document.querySelector.bind(document)
-const $$ = document.querySelectorAll.bind(document)
-
-$$("[class^='section-number-']").forEach((section) => {
-  const linkTarget = section.parentNode.id.match(/^org/) ? section.innerText : section.parentNode.id
-  section.innerHTML = `<a href="#${linkTarget}">§<a name="${linkTarget}"></a></a>`
-})
