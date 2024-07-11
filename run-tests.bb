8c0e2a59b2de245556316efb2d2fa6f7241f9160
Jakub Šťastný <jakub-stastny@users.noreply.github.com>
2024-07-11 13:30:55 -0600

    Remove from branch

--- run-tests.bb
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bb
-
-;; Run: bb -cp "$(clojure -Spath)" run-tests.bb
-
-(require '[clojure.test :refer :all]
-         '[integration-test :refer :all])
-
-(run-tests 'integration-test)
--- test/jakub_stastny/et/parser_test.clj
+++
@@ -1,9 +0,0 @@
-(ns jakub-stastny.et.parser-test
-  (:require [clojure.test :refer :all]
-            [jakub-stastny.et.parser :as parser]))
-
-(deftest example-test
-  (testing "Example test case"
-    ;(is (= (parser/parse "some input") "expected output"))
-    (println "Hey there!")
-    ))
